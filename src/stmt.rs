7ed348b6a1d475f04f36b2720726af648aa4ce61
Lakshay <lakshay21060@iiitd.ac.in>
2023-12-17 00:03:36 +0530

    started parsing stmts

--- src/stmt.rs
+++ src/stmt.rs
@@ -6,15 +6,21 @@ pub struct Block {
     vars: HashMap<String, Expr>,
 }
 
-struct FunSign {
+pub enum Type {
+    Str,
+    Int,
+    Bool,
+}
+
+pub struct Arg {
     name: String,
-    args: Vec<Expr>,
+    arg_type: Type, 
 }
 
 pub struct FunDefStmt {
     name: String,
-    args: Vec<Expr>,
-    return_exrp: Expr,
+    args: Vec<Arg>,
+    return_exrp: Type,
     block: Block,
 }
 
@@ -43,14 +49,60 @@ pub enum Stmt {
     CF(CFStmt),
 }
 
+fn token_kind_to_type(kind: i32) -> Option<Type> {
+    match kind {
+        lexer::TOK_TYPE_INT => Some(Type::Int),
+        lexer::TOK_TYPE_STR => Some(Type::Str),
+        lexer::TOK_TYPE_BOOL => Some(Type::Bool),
+        _ => None
+    }
+}
+
+fn parse_type(lexer: &mut Lexer) -> Type {
+    let x: Option<Type> = token_kind_to_type(lexer.front().kind);
+    if x.is_none() {lexer.error(String::from("invalid type"), None);}
+    lexer.eat(); // eat type
+    x.unwrap()
+}
+
+fn parse_block(lexer: &mut Lexer) -> Option<Stmt> {
+    // TODO: to be implemented
+    None
+}
+
+fn parse_stmt(lexer: &mut Lexer) -> Option<Stmt> {
+    // TODO: to be implemented
+    None
+}
 
 fn parse_fun_def(lexer: &mut Lexer) -> Option<Stmt> {
     lexer.assert_token_kind(lexer::TOK_FN);
     lexer.eat(); // eat fn
     lexer.assert_token_kind(lexer::TOK_IDEN);
-    let name: String = lexer.front().get_str_val()
-        .unwrap().to_string();
+    let name: String = lexer.front()
+        .get_str_val().unwrap().to_string();
     lexer.eat(); // eat name
-    
+    lexer.assert_token_kind('(' as i32);
+    lexer.eat(); // eat '('
+    let mut args: Vec<Arg> = Vec::new();
+    while !lexer.empty() && lexer.is_token_kind(')' as i32) {
+        lexer.assert_token_kind(lexer::TOK_IDEN);
+        let arg_name: String = lexer.front()
+            .get_str_val().unwrap().to_string();
+        lexer.eat(); // eat arg_name
+        lexer.assert_token_kind(':' as i32);
+        lexer.eat(); // eat ':'
+        lexer.assert_token();
+        let arg_type: Type = parse_type(lexer);
+        args.push(Arg{name: arg_name, arg_type: arg_type});
+        if !lexer.empty() && lexer.is_token_kind(')' as i32) {break;}
+        lexer.assert_token_kind(',' as i32);
+        lexer.eat(); // eat ','
+    }    
+    lexer.eat(); // eat ')'
+    lexer.assert_token_kind(':' as i32);
+    lexer.eat(); // eat ':'
+    let return_type: Type = parse_type(lexer);
+    // TODO: parse block
     None   
 }
