8da96906eb4c4b25f821a9077ba638ac6b36bb07
Lakshay <lakshay21060@iiitd.ac.in>
2023-12-20 14:40:42 +0530

    added new dependency 'clap'

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,44 @@
 use std::{io::{prelude::*, BufReader}};
+use clap::{Arg, Command, ArgAction, ArgGroup};
 
 pub mod fe;
 pub mod transpiler_cpp;
 
 use fe::{lexer::Lexer, parser::parse_lexer, stmt::Stmt};
 use transpiler_cpp::transpiler;
+
+fn cli() -> Command {
+    Command::new("kappa")
+        .about("Programming language made just for fun")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            Command::new("compile")
+                .about("Compile source code to target(s)")
+                .arg_required_else_help(true)
+                .arg(Arg::new("cpp")
+                    .long("cpp")
+                    .required(false)
+                    .action(ArgAction::SetTrue)
+                    .help("Target C++"))
+                .arg(Arg::new("felf64")
+                    .long("felf64")
+                    .required(false)
+                    .action(ArgAction::SetTrue)
+                    .help("Target felf64"))
+                .group(ArgGroup::new("targets")
+                    .args(["cpp", "felf64"])
+                    .required(true)
+                    .multiple(true))
+                .arg(Arg::new("filepath")
+                    .short('f')
+                    .long("filepath")
+                    .required(true)
+                    .action(ArgAction::Append)
+                    .help("Source file path"))
+    )
+}
 
 fn print_usage() {
     println!("usage: kappa <subcommand> [<args>]");
@@ -22,9 +56,12 @@ fn get_content(filepath: &str) -> Vec<String> {
 }
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 3 { print_usage(); }
-    let filepath: &str  = &args[2];
+    let match_result = cli().get_matches();
+    let compile_args = match_result.subcommand_matches("compile");
+    let filepath: String = compile_args.unwrap()
+        .get_one::<String>("filepath").unwrap().to_string();
+    let cpp: bool = *compile_args.unwrap().get_one::<bool>("cpp").unwrap();
+    let felf: bool = *compile_args.unwrap().get_one::<bool>("felf64").unwrap();
     let content: Vec<String> = get_content(&filepath);
     let mut lexer: Lexer = Lexer::new(content, filepath.to_string());
     lexer.print();
@@ -34,5 +71,5 @@ fn main() {
         println!("{}", stmt);
     }
     println!("");
-    transpiler(String::from("eg/main.cpp"), stmts);
+    if cpp {transpiler(String::from("eg/main.cpp"), stmts);}
 }
